2ef2d3a3816a5e6f180e2a94550c4c54afa6fb08
Obramenko Alexandr <obram22@gmail.com>
2018-01-28 14:04:57 +0200

    animate width

--- elementWidth/js/main.js
+++ elementWidth/js/main.js
@@ -1,9 +1,37 @@
 elementWidthAnimate('element-1', '200', '1000');
 
+
+// Передаем 3 параметра (id элемента, ширина, время анимации)
 function elementWidthAnimate(element, width, time) {
-	var currentElement = document.getElementById(element);
-	console.log(currentElement);
-	setTimeout(function() {
-		currentElement.style.width = width + 'px';
-	}, time);
+    if (element && width && time) {
+        currentElement = document.getElementById(element);
+        animate({
+            duration: time,
+            timing: function(timeFraction) {
+                return timeFraction;
+            },
+            draw: function(progress) {
+                currentElement.style.width = progress * width + 'px';
+            }
+        });
+    } else {
+    	alert('Передайте параметры анимирования элемента!');
+    }
+}
+
+// функция анимации , основана на requestAnimationFrame(нагуглил :) )
+function animate(options) {
+    var start = performance.now();
+    requestAnimationFrame(function animate(time) {
+        var timeFraction = (time - start) / options.duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        var progress = options.timing(timeFraction)
+
+        options.draw(progress);
+
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+    });
 }
