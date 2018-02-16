8b31d482c0b713edcdd0d5fa8c92fbe0cafc6749
Obramenko Alexandr <obram22@gmail.com>
2018-02-16 23:52:53 +0200

    tooltip

--- tooltip/js/main.js
+++ tooltip/js/main.js
@@ -5,19 +5,18 @@ $(function() {
   var tooltipText = link.attr('title');
 
   link.mouseenter(function(e) {
-    // Убираем стандартный title
+    // Убираем стандартный title , записываем его значение во временный атрибут
     var title = $(this).attr("title");
     $(this).attr("tmp_title", title);
     $(this).attr("title","");
     tooltip.text(title).show();
-
   });
 
   link.mouseout(function(e) {
     tooltip.hide();
     var title = $(this).attr("tmp_title");
 
-    // Возвращаем title на место
+    // Возвращаем title на место и удаляем временный атрибут
     $(this).attr("title", title);
     $(this).removeAttr('tmp_title');
   });
@@ -31,4 +30,5 @@ $(function() {
       'left': xCoord
     });
   });
+
 });
